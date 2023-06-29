2b2c1818d75249a9e597063859895b197b1a02e4
Joe <joe@blockbuilders.co.uk>
2023-06-29 13:53:44 +0100

    Added npcreload to the script

--- ClickMe.py
+++ ClickMe.py
@@ -1,4 +1,3 @@
-
 ############################################### Imports dependencies ##########################################################################################################################################
 import pandas as pd
 import json
@@ -8,7 +7,8 @@ import json
 #get the location of the scriptss
 import os
 script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
-rel_path = "*.xlsx"
+
+rel_path = "npc.xlsx"
 abs_file_path = os.path.join(script_dir, rel_path)
 
 ##############################################  Inputs ###################################################################################################Minecraft_version = 1.17
@@ -43,6 +43,7 @@ scene = [] #Prepares a list for appending later on
 
 text = [] #Prepares a list for the text to be added later on. 
 
+npcreload = [] #Prepares a list for the text to be added later on.
 ############################################## Runs through the rows formatting a json file ##########################################################################################################################################
 
 for row in npc_template: # Runs through the rows and creates a new list
@@ -82,10 +83,11 @@ for row in npc_template: # Runs through the rows and creates a new list
     scene.append({'scene_tag':npc_template.iloc[:,0][row_count], 'npc_name': npc_template.iloc[:,2][row_count], 'text':npc_template.iloc[:,3][row_count], 'on_open_commands': on_open_commands[row_count], 'on_close_commands': on_close_commands[row_count], "buttons": [buttons1, buttons2, buttons3, buttons4, buttons5, buttons6]})
     #writes the text file lines for each row in the excel.
     
-    string = f"######################################################## \n INSTRUCTIONS FOR: \n NAME = {npc_template.iloc[:,2][row_count]} \n TAG = {npc_template.iloc[:,0][row_count]} \n \n 1. Stand in front of the NPC and copy this command: \n \n /tag @e[type=npc, r=2] add {npc_template.iloc[:,0][row_count]} \n \n 2. Then paste this command: \n \n /dialogue change @e[tag={npc_template.iloc[:,0][row_count]}] {npc_template.iloc[:,2][row_count]} \n \n ######################################################## \n "
-    
-    
+    string = f"######################################################## \n INSTRUCTIONS FOR: \n NAME = {npc_template.iloc[:,2][row_count]} \n TAG = {npc_template.iloc[:,0][row_count]} \n \n 1. Stand in front of the NPC and copy this command: \n \n /tag @e[type=npc, r=2] add {npc_template.iloc[:,0][row_count]} \n \n 2. Then paste this command: \n \n /dialogue change @e[tag={npc_template.iloc[:,0][row_count]}] {npc_template.iloc[:,2][row_count]} \n \n ######################################################## \n "    
     text.append(string)
+
+    NPC_reload = f"dialogue change @e[{npc_template.iloc[:,0][row_count]}] {npc_template.iloc[:,2][row_count]}"
+    npcreload.append(NPC_reload)
     
     row_count = row_count + 1 # Increases the row count by 1
 
@@ -98,9 +100,11 @@ scene = {"format_version": Minecraft_version, "minecraft:npc_dialogue":{"scenes"
 with open(f'{os.path.dirname(__file__)}/dialogue/scene.json', 'w', encoding='utf-8') as f:
     json.dump(scene, f, ensure_ascii=False, indent=4)
 
-with open(f"{os.path.dirname(__file__)}/dialogue/README.txt", "w") as f:
+with open(f"{os.path.dirname(__file__)}/README.txt", "w") as f:
     f.write("\n".join(text))
-    
+
+with open(f"{os.path.dirname(__file__)}/reloadNPC.mcfunction", "w") as f:
+    f.write("\n".join(npcreload))
     
 
 
