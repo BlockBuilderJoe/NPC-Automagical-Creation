991ebf67765a231f88959c2471e5dd91e34966c5
Joe <joe@blockbuilders.co.uk>
2024-01-11 13:08:30 +0000

    Added localisation feature

--- ClickMe.py
+++ ClickMe.py
@@ -40,50 +40,81 @@ row_count = 0  # Creates the row count for reference
 scene_amount = len(npc_template)
 
 scene = [] #Prepares a list for appending later on  
+translated_lines = [] #Prepares a list for appending later on
 
 text = [] #Prepares a list for the text to be added later on. 
 
 npcreload = [] #Prepares a list for the text to be added later on.
 ############################################## Runs through the rows formatting a json file ##########################################################################################################################################
 
-for index, row in npc_template.iterrows(): # Runs through the rows and creates a new list
-    print(row_count)
+for index, row in npc_template.iterrows():
+    #Appends the lines to the list if they are not empty, this is for the en_US.lang file.
+    if npc_template.iloc[:,2][row_count]:
+        translated_lines.append(npc_template.iloc[:,2][row_count] + "=")
+    if npc_template.iloc[:,3][row_count]:
+        translated_lines.append(npc_template.iloc[:,3][row_count] + "=")
+    if npc_template.iloc[:,6][row_count]:
+        translated_lines.append(npc_template.iloc[:,6][row_count] + "=")
+    if npc_template.iloc[:,8][row_count]:
+        translated_lines.append(npc_template.iloc[:,8][row_count] + "=")
+    if npc_template.iloc[:,10][row_count]:
+        translated_lines.append(npc_template.iloc[:,10][row_count] + "=")
+    if npc_template.iloc[:,12][row_count]:
+        translated_lines.append(npc_template.iloc[:,12][row_count] + "=")
+    if npc_template.iloc[:,14][row_count]:
+        translated_lines.append(npc_template.iloc[:,14][row_count] + "=")
+    if npc_template.iloc[:,16][row_count]:
+        translated_lines.append(npc_template.iloc[:,16][row_count] + "=")
+    #Formats the json translation for localisation.
+    npc_name_translate = {"rawtext": [{"translate": npc_template.iloc[:,2][row_count]}]}
+    text_translate = {"rawtext": [{"translate": npc_template.iloc[:,3][row_count]}]}
+    button1_translate = {"rawtext": [{"translate": npc_template.iloc[:,6][row_count]}]}
+    button2_translate = {"rawtext": [{"translate": npc_template.iloc[:,8][row_count]}]}
+    button3_translate = {"rawtext": [{"translate": npc_template.iloc[:,10][row_count]}]}
+    button4_translate = {"rawtext": [{"translate": npc_template.iloc[:,12][row_count]}]}
+    button5_translate = {"rawtext": [{"translate": npc_template.iloc[:,14][row_count]}]}
+    button6_translate = {"rawtext": [{"translate": npc_template.iloc[:,16][row_count]}]}
+
     buttons1 = {
-        "name": npc_template.iloc[:,6][row_count], 
+        "name": button1_translate, 
         "commands": commands_1[row_count]
     }
     buttons2 = {
-        "name": npc_template.iloc[:,8][row_count], 
+        "name": button2_translate, 
         "commands": commands_2[row_count]
     }
     buttons3 = {
-        "name": npc_template.iloc[:,10][row_count], 
+        "name": button3_translate, 
         "commands": commands_3[row_count]
     }
     buttons4 = {
-        "name": npc_template.iloc[:,12][row_count], 
+        "name": button4_translate, 
         "commands": commands_4[row_count]
     }
     buttons5 = {
-        "name": npc_template.iloc[:,14][row_count], 
+        "name": button5_translate, 
         "commands": commands_5[row_count]
     }
     buttons6 = {
-        "name": npc_template.iloc[:,16][row_count], 
+        "name": button6_translate, 
         "commands": commands_6[row_count]
     }
+
     #replace apostrophes with apostrophes
     npc_template.iloc[:,3][row_count] = npc_template.iloc[:,3][row_count].replace("’", "'")
     npc_template.iloc[:,3][row_count] = npc_template.iloc[:,3][row_count].replace("‘", "'")
     npc_template.iloc[:,3][row_count] = npc_template.iloc[:,3][row_count].replace(" ", "")
     npc_template.iloc[:,3][row_count] = npc_template.iloc[:,3][row_count].replace("...", " . . .")
 
-
-    
     #writes the json file
-    scene.append({'scene_tag':npc_template.iloc[:,0][row_count], 'npc_name': npc_template.iloc[:,2][row_count], 'text':npc_template.iloc[:,3][row_count], 'on_open_commands': on_open_commands[row_count], 'on_close_commands': on_close_commands[row_count], "buttons": [buttons1, buttons2, buttons3, buttons4, buttons5, buttons6]})
-    #writes the text file lines for each row in the excel.
-    
+    scene.append({
+        'scene_tag': npc_template.iloc[:,0][row_count], 
+        'npc_name': npc_name_translate, 
+        'text': text_translate, 
+        'on_open_commands': on_open_commands[row_count], 
+        'on_close_commands': on_close_commands[row_count], 
+        "buttons": [buttons1, buttons2, buttons3, buttons4, buttons5, buttons6]
+    })
     string = f"######################################################## \n INSTRUCTIONS FOR: \n NAME = {npc_template.iloc[:,2][row_count]} \n TAG = {npc_template.iloc[:,1][row_count]} \n \n 1. Stand in front of the NPC and copy this command: \n \n /tag @e[type=npc, r=2] add {npc_template.iloc[:,1][row_count]} \n \n 2. Then paste this command: \n \n /dialogue change @e[tag={npc_template.iloc[:,1][row_count]}] {npc_template.iloc[:,0][row_count]} \n \n ######################################################## \n "    
     text.append(string)
 
@@ -107,7 +138,9 @@ with open(f"{os.path.dirname(__file__)}/README.txt", "w") as f:
 with open(f"{os.path.dirname(__file__)}/reloadNPC.mcfunction", "w") as f:
     f.write("\n".join(npcreload))
     
-
+# Write the translated lines for the language file.
+with open(f"{os.path.dirname(__file__)}/translated_lines.txt", "w") as f:
+    f.write("\n".join(translated_lines))
 
     
 
